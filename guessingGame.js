aab535ec4fbe33bf89b8ac884135c3ee91e08527
Joginder Dhaliwal <joginderdhaliwal@Joginders-MacBook-Air.local>
2026-05-11 13:51:59 -0400

    Added user input

--- guessingGame.js
+++ guessingGame.js
@@ -1,10 +1,22 @@
-let secretNumber = Math.floor(Math.random() * 20) + 1;
-let guess = 10;
+const readline = require("readline");
 
-if(guess > secretNumber){
-    console.log("Too High");
-} else if (guess === secretNumber) {
-    console.log("Correct!");
-} else {
-    console.log("Too Low");
-}
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+let secretNumber = Math.floor(Math.random() * 20) + 1;
+
+rl.question("Enter your guess: ", function(answer) {
+    let guess = Number(answer);
+
+    if (guess > secretNumber) {
+        console.log("Too High");
+    } else if (guess === secretNumber) {
+        console.log("Correct!");
+    } else {
+        console.log("Too Low");
+    }
+
+    rl.close();
+});
